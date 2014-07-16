5b4ddc8e6183cf31c4d752dee713e1895a897f7d
mickael <caz0@hotmail.fr>
2014-07-16 20:50:14 +0200

    Hello World

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 16 juil. 2014
+ *      Author: mickael
+ */
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+	std::cout << "Hello World !" << std::endl;
+}
+
+
